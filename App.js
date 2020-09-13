aff8549ed3cfa048be40f1d308c66d0c3bd2958e
sjaugmented <sjaugmented@gmail.com>
2020-09-13 09:55:33 -0700

    touchables

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, Image, Alert, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
-  const handlePress = () => {
-    console.log('text pressed')
-  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-      Hello World! A really really long text to see what happens when it's waaaaaaaaaay too long</Text>
+      <Button 
+      color='red'
+      title='Click Me' 
+      onPress={() => 
+        Alert.prompt('My title', 'My message', text => console.log(text))
+      } />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -19,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(255, 170, 0)',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
