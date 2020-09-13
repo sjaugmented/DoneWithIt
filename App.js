e206215b7767028e84474d4ffb146483c82297dd
sjaugmented <sjaugmented@gmail.com>
2020-09-13 09:16:41 -0700

    text component

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  let x = 1
+  const handlePress = () => {
+    console.log('text pressed')
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+      Hello World! A really really long text to see what happens when it's waaaaaaaaaay too long</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgb(255, 170, 0)',
   },
 });
